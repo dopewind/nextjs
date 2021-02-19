0a86913c0ad52118e6a237c3f7635d824c4d598f
Restyled.io <commits@restyled.io>
2021-02-19 10:27:05 +0000

    Restyled by prettier

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,18 +1,16 @@
-const projectsData =
-    [
-      {
-        title : 'This thing',
-        description :
-            `This thing you are reading is probably the best thing I have to showcase`,
-        imgSrc : '/static/images/vercel.png',
-        href : 'https://www.github.com/dopewind/litblog/',
-      },
-      {
-        title : 'The dope site',
-        description : `Another random thing I made`,
-        imgSrc : '/static/images/portfolio.jpg',
-        href : 'https://www.dopewind.github.io/',
-      },
-    ]
+const projectsData = [
+  {
+    title: 'This thing',
+    description: `This thing you are reading is probably the best thing I have to showcase`,
+    imgSrc: '/static/images/vercel.png',
+    href: 'https://www.github.com/dopewind/litblog/',
+  },
+  {
+    title: 'The dope site',
+    description: `Another random thing I made`,
+    imgSrc: '/static/images/portfolio.jpg',
+    href: 'https://www.dopewind.github.io/',
+  },
+]
 
-    export default projectsData
+export default projectsData
